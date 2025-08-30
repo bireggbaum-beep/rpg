b4ee3041fdc87f6c32a5727d6c74149251c2e5a2
bireggbaum-beep <biregg.baum@gmail.com>
2025-08-30 16:35:21 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import { $, txt, plusify, formatNote, parseRes, fmtBew, formatPaNote, normalizeHg } from './utils.js';
 import { naturalAttackLine, renderLoot } from './render.js';
-import { filterAndSort, populateList } from './list.js';
 import { filterAndSort, populateList, populateGroupedList } from './list.js';
 
 document.addEventListener("DOMContentLoaded", () => {
